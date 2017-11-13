9fc91117598bb002c6dde838d0d7b574748534f3
Malcolm Jones <malcolm@adobe.com>
2017-11-12 23:52:43 -0500

    Part 15: mapping news data

--- gotuts/gotut15_mapping_news_data.go
+++ gotuts/gotut15_mapping_news_data.go
@@ -1,30 +1,82 @@
 package main
 
-import "fmt"
+// If we didn't have the washtion post website available, we could uncomment this below
+/*
+var washPostXML = []byte(`
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-politics-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-blogs-politics-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-opinions-sitemap.xml</loc>
+   </sitemap>
+</sitemapindex>`)
+*/
+
+import ("fmt"
+		"net/http"
+		"io/ioutil"
+		"encoding/xml")
+
+type SitemapIndex struct {
+	// MUST capitalize values, otherwise they won't get exported, and you won't be able to use them
+	// sitemap>loc: location tag under the sitemap tag
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword string
+	Location string
+}
 
 func main() {
-	// key is string, value is float32. Think dictonary in python
-	// Refactor 1
-	// var grades map[string]float32
 
-	// Refactor 2
-	// NOTE: A map usually doesn't have any values. To initialize one, we use golang built-in make command
-	Grades := make(map[string]float32)
+	// Create instance of struct SitemapIndex ( kinda like initalizing a class )
+	var s SitemapIndex
+	var n News
 
-	Grades["Timmy"] = 42
-	Grades["Jess"] = 92
-	Grades["Sam"] = 67
-	fmt.Println(Grades)
+	// NOTE: Create a map where the key is a string, and the values are of type struct NewsMap
+	news_map := make(map[string]NewsMap)
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
 
-	TimsGrade := Grades["Timmy"]
-	fmt.Println(TimsGrade)
+	// Unmarshall values, from the body response, and send it to memory address of SitemapIndex instance s
+	xml.Unmarshal(bytes, &s)
 
-	delete(Grades, "Timmy")
-	fmt.Println(Grades)
+	// Grab data from each news link in sitemap
+	fmt.Printf("Here %s some %s", "are", "variables")
+	for _, Location := range s.Locations {
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
 
-	// Iterate through map
-	for k, v := range Grades {
-		fmt.Println(k,":",v)
+		// Unmarshal maps an XML element to a Name by recording the element name.
+		// Unmarshal values, from the body response, and send it to memory address of SitemapIndex instance s
+		xml.Unmarshal(bytes, &n)
+
+		// idx means index
+		for idx, _ := range n.Titles {
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
 	}
 
+	// Print out data now
+	for idx, data := range news_map {
+		fmt.Println("\n\n\n", idx)
+		fmt.Println("\n", data.Keyword)
+		fmt.Println("\n", data.Location)
+	}
 }
+
+
+// Next tutorial, we'll build a map then display to page
